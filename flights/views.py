b025df48f57f192c3e59f171259de8c6648a9b89
IsuruFerna <isurumadushan.ferna@gmail.com>
2023-03-18 21:37:47 +0100

    superuser/admin added

--- flights/views.py
+++ flights/views.py
@@ -7,3 +7,10 @@ def index(request):
     return render(request, "flights/index.html", {
       "flights": Flight.objects.all()  
     })
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(pk=flight_id)
+    print(flight.id, flight.destination)
+    return render(request, "flights/flight.html", {
+        "flight": flight
+    })
